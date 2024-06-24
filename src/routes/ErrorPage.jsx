cd3e2864070593302099a72f37562b87393c9019
LuizEdu-AR <luiz.rodrigues67246@alunos.ufersa.edu.br>
2024-06-24 13:31:31 -0300

    pequenas alterações

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -2,6 +2,8 @@ import { useRouteError } from "react-router-dom";
 
 import './ErrorPage.css';
 
+import GLogo from "../assets/img/g-header.png"
+
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -9,9 +11,11 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
+      <img src={GLogo} alt="G-Corp" />
       <h1>Oops!</h1>
       <p>Temos um problema.</p>
       <p>Já estamos resolvendo, aguarde por favor...</p>
+      <p>Toda a equipe da G-Corp pede perdão pelo ocorrido.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
